56efb4b31850e0e0f84be8248ec8f980f0654b6d
Artur <piszczatowski.artur@gmail.com>
2022-10-16 18:59:15 +0200

    CRUD set-up

---
+++ d-gen/src/test/java/pjait/dgen/DGenApplicationTests.java
@@ -0,0 +1,13 @@
+package pjait.dgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
