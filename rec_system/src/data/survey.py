851ea63f9a8beafb4414e848fcd0e5b3e8ccf171
unknown <m.kucha@wp.pl>
2022-11-20 23:58:24 +0100

    [PIF-14] first recommendation tools created, improved data management

--- rec_system/src/data/survey.py
+++ rec_system/src/data/survey.py
@@ -1,55 +1,52 @@
 import pandas as pd
 
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.max_rows', 500)
-
-COLUMNS_TO_DROP = ['W skali od 1 do 10 jak bardzo lubisz słone jedzenie', 'W skali od 1 do 10 jak bardzo lubisz słodkie jedzenie',
-         'W skali od 1 do 10 jak bardzo lubisz gorzkie jedzenie', 'W skali od 1 do 10 jak bardzo lubisz mięso',
-         'W skali od 1 do 10 jak bardzo lubisz pikantne jedzenie',
-         'W skali od 1 do 10 jak bardzo lubisz kuchnie śródziemnomorską',
-         'W skali od 1 do 10 jak bardzo lubisz kuchnie azjatycką',
-         'W skali od 1 do 10 jak bardzo lubisz kuchnie amerykańską',
-         'W skali od 1 do 10 jak bardzo lubisz kuchnie arabską',
-         'W skali od 1 do 10 jak bardzo lubisz owoce morza(również ryby)',
-         'W skali od 1 do 10 jak bardzo lubisz tłuste jedzenie', 'W skali od 1 do 10 jak bardzo lubisz kuchnie Polską'
-         ]
+COLUMNS_TO_DROP = ['W skali od 1 do 10 jak bardzo lubisz słone jedzenie',
+                   'W skali od 1 do 10 jak bardzo lubisz słodkie jedzenie',
+                   'W skali od 1 do 10 jak bardzo lubisz gorzkie jedzenie',
+                   'W skali od 1 do 10 jak bardzo lubisz mięso',
+                   'W skali od 1 do 10 jak bardzo lubisz pikantne jedzenie',
+                   'W skali od 1 do 10 jak bardzo lubisz kuchnie śródziemnomorską',
+                   'W skali od 1 do 10 jak bardzo lubisz kuchnie azjatycką',
+                   'W skali od 1 do 10 jak bardzo lubisz kuchnie amerykańską',
+                   'W skali od 1 do 10 jak bardzo lubisz kuchnie arabską',
+                   'W skali od 1 do 10 jak bardzo lubisz owoce morza(również ryby)',
+                   'W skali od 1 do 10 jak bardzo lubisz tłuste jedzenie',
+                   'W skali od 1 do 10 jak bardzo lubisz kuchnie Polską',
+                   'W skali od 1 do 10 jak bardzo lubisz kwaśne jedzenie'
+                   ]
 SHEET_IDS = ['1yUzQLGwdEn0zUZj7c_y5dnJi4hxk4wGQltBSxk3ZUOE', '1NxPcU-3V44ym9Slsh-HvRDDIgHzV59ySODvAzaDf9Nw',
-                 '147TGE1t-TuIcVVcdlhJnweC1gYb1LpM7_q-ui_c9iHg', '1yBgvpb6DgHUhnDyRFDfqDU5S26CPHFws7Ice7A6V1jE',
-                 '1m1Ts2wLX38jup3wQOp2k0HW63ebewiUXEjFus23vPuo', '1mIsMsOw1jGCuFTzPZyw0wLZyRqqD2m32SGiN1DyNH90',
-                 '1RVm-aa35TxOE8f7uxlqAhTk-Lr2Kw88ehgiIeDcuUsk', '1I3JDwCXwZaWBmLUjfp_257epaUHqrk1o5vhoPPdYKdM',
-                 '19EoiUu8yB4637pyEBKQdC-RYCNWcvjktxYy975zc9iU', '1ghiqVoaMkO5F8Qh7A90N8sMmouh1l5sxpkDUlTgW75E',
-                 '1LVWX9ODt5nScHKuWpACEkwAbV1OtKI-XNIG5-CQ4dJc', '18Hx-gjtKAJh0Mm-M2wuNR0N2tp1Tlh_r1iBEnAcfHbE',
-                 '14Jjv6nYrrbtke4zG5rBMavOyh8mhlrDSzjjvFGmunXE', '1V64x4Rvhfq0gRAEt5DEh9SylmAyaSDPSVuk6DFGNUWs',
-                 '171mWbBJroQRjPYJRHB5CimWULBZcywDTSocv2YRtRWI', '1XZXBL76r8VY3IHnJuXOQ_eTYDVjvTONyEtdeISVUdok',
-                 '1pT8nYF272bnO28H_Ttm1ziGcf2bLQd02Gy-x_dKa5d8', '1N2w2MAv9Oun5zLCgdB4QmDX6FlPpQXLK_cTKVCJfgAw',
-                 '1ChaPNihN0HsVQRLmiwuQ9GA_8DL7FmGumq0MYgRX7AA', '1FtIGUXp1H52Nc9tYPPAMu98zcW0za6s5hsGAyKC-RJc']
+             '147TGE1t-TuIcVVcdlhJnweC1gYb1LpM7_q-ui_c9iHg', '1yBgvpb6DgHUhnDyRFDfqDU5S26CPHFws7Ice7A6V1jE',
+             '1m1Ts2wLX38jup3wQOp2k0HW63ebewiUXEjFus23vPuo', '1mIsMsOw1jGCuFTzPZyw0wLZyRqqD2m32SGiN1DyNH90',
+             '1RVm-aa35TxOE8f7uxlqAhTk-Lr2Kw88ehgiIeDcuUsk', '1I3JDwCXwZaWBmLUjfp_257epaUHqrk1o5vhoPPdYKdM',
+             '19EoiUu8yB4637pyEBKQdC-RYCNWcvjktxYy975zc9iU', '1ghiqVoaMkO5F8Qh7A90N8sMmouh1l5sxpkDUlTgW75E',
+             '1LVWX9ODt5nScHKuWpACEkwAbV1OtKI-XNIG5-CQ4dJc', '18Hx-gjtKAJh0Mm-M2wuNR0N2tp1Tlh_r1iBEnAcfHbE',
+             '14Jjv6nYrrbtke4zG5rBMavOyh8mhlrDSzjjvFGmunXE', '1V64x4Rvhfq0gRAEt5DEh9SylmAyaSDPSVuk6DFGNUWs',
+             '171mWbBJroQRjPYJRHB5CimWULBZcywDTSocv2YRtRWI', '1XZXBL76r8VY3IHnJuXOQ_eTYDVjvTONyEtdeISVUdok',
+             '1pT8nYF272bnO28H_Ttm1ziGcf2bLQd02Gy-x_dKa5d8', '1N2w2MAv9Oun5zLCgdB4QmDX6FlPpQXLK_cTKVCJfgAw',
+             '1ChaPNihN0HsVQRLmiwuQ9GA_8DL7FmGumq0MYgRX7AA', '1FtIGUXp1H52Nc9tYPPAMu98zcW0za6s5hsGAyKC-RJc']
 
-def update_survey_answers_file():
+
+def update_survey_answers_file(to_csv: bool = True, destination: str = "./data/survey.csv"):
     sheet_ids = SHEET_IDS
     df = pd.DataFrame({})
     for sheet_id in sheet_ids:
         tmp_df = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
         df = pd.concat([df, tmp_df])
     df = df.drop(['Sygnatura czasowa'], axis=1)
-    df.to_csv('./data/survey.csv', index=False)
-
-
-def parse_survey_answers_files():
-    df = pd.read_csv('./data/survey.csv')
-    df.dropna()
-    df = df.drop(COLUMNS_TO_DROP, axis=1)
-    columns = df.columns
-    df.T.insert(0, "questions", columns)
-    df = df.pivot_table(index='Adres e-mail')
+    if to_csv:
+        df.to_csv(destination, index=False)
     return df
 
 
-def convert_to_implicit_dataset():
-    modified_dict = dict()
-    df = pd.read_csv('./data/survey.csv')
+def convert_to_implicit_dataset(df: pd.DataFrame = None, survey_path: str = './data/survey.csv', to_csv: bool = False,
+                                destination: str = "./data/ratings.csv"):
+    if not df:
+        df = pd.read_csv(survey_path)
     df.dropna()
     df = df.drop(COLUMNS_TO_DROP, axis=1)
     df_dict = df.to_dict()
+    modified_dict = dict()
+
     i = 0
     for question, ratings_dict in df_dict.items():
         if question == 'Adres e-mail':
@@ -61,58 +58,7 @@ def convert_to_implicit_dataset():
                 'ocena': rating
             }
             i += 1
-
-    return pd.DataFrame.from_dict(modified_dict).T
-
-
-
-
-
-
-
-
-
-
-
-
-# import pandas as pd
-# import numpy as np
-# ratings_matrix = pd.read_csv('./data/survey.csv')
-# ratings_matrix.dropna()
-# ratings_matrix = ratings_matrix.drop(
-#     ['W skali od 1 do 10 jak bardzo lubisz słone jedzenie', 'W skali od 1 do 10 jak bardzo lubisz słodkie jedzenie',
-#      'W skali od 1 do 10 jak bardzo lubisz gorzkie jedzenie', 'W skali od 1 do 10 jak bardzo lubisz mięso',
-#      'W skali od 1 do 10 jak bardzo lubisz pikantne jedzenie',
-#      'W skali od 1 do 10 jak bardzo lubisz kuchnie śródziemnomorską',
-#      'W skali od 1 do 10 jak bardzo lubisz kuchnie azjatycką',
-#      'W skali od 1 do 10 jak bardzo lubisz kuchnie amerykańską',
-#      'W skali od 1 do 10 jak bardzo lubisz kuchnie arabską',
-#      'W skali od 1 do 10 jak bardzo lubisz owoce morza(również ryby)',
-#      'W skali od 1 do 10 jak bardzo lubisz tłuste jedzenie', 'W skali od 1 do 10 jak bardzo lubisz kuchnie Polską'
-#      ],
-#     axis=1)
-# ratings_matrix = ratings_matrix.pivot_table(index='Adres e-mail', fill_value=0)
-# ratings_matrix.shape
-# X = ratings_matrix.T
-# X.head()
-# X.shape
-# X1 = X
-# from sklearn.decomposition import TruncatedSVD
-# SVD = TruncatedSVD(n_components=10)
-# decomposed_matrix = SVD.fit_transform(X)
-# decomposed_matrix.shape
-# correlation_matrix = np.corrcoef(decomposed_matrix)
-# correlation_matrix.shape
-# X.index[75]
-# i = 'Frikadellen German Meat Patties'
-# product_names = list(X.index)
-# product_ID = product_names.index(i)
-# product_ID
-# correlation_product_ID = correlation_matrix[product_ID]
-# correlation_product_ID.shape
-# Recommend = list(X.index[correlation_product_ID > 0.65])
-#
-# # Removes the item already bought by the customer
-# Recommend.remove(i)
-#
-# Recommend[0:24]
+    df = pd.DataFrame.from_dict(modified_dict).T
+    if to_csv:
+        df.to_csv(destination, index=True)
+    return df
--- rec_system/src/recommender.py
+++ rec_system/src/recommender.py
@@ -1,16 +1,11 @@
 from implicit.als import AlternatingLeastSquares
-from implicit.lmf import LogisticMatrixFactorization
-from implicit.bpr import BayesianPersonalizedRanking
 from implicit.nearest_neighbours import bm25_weight
 from scipy.sparse import csr_matrix
-from typing import Dict, Any, Union
-import numpy as np
-import pandas as pd
+from typing import Dict, Any
+
 
 MODEL = {
-    "lmf": LogisticMatrixFactorization,
     "als": AlternatingLeastSquares,
-    "bpr": BayesianPersonalizedRanking,
 }
 
 
@@ -43,42 +38,22 @@ class Recommender:
         self.user_idx = user_idx
         self.product_idx = product_idx
 
-        # This variable will be set during training phase
         self.model = None
         self.fitted = False
 
     def create_and_fit(
             self,
             model_name: str,
-            weight_strategy: str = "bm25",
             model_params: Dict[str, Any] = {},
     ):
-        weight_strategy = weight_strategy.lower()
-        if weight_strategy == "bm25":
-            data = bm25_weight(
-                self.user_product_matrix,
-                K1=1.2,
-                B=0.75,
-            )
-        elif weight_strategy == "balanced":
-            # Balance the positive and negative (nan) entries
-            # http://stanford.edu/~rezab/nips2014workshop/submits/logmat.pdf
-            total_size = (
-                    self.user_product_matrix.shape[0] * self.user_product_matrix.shape[1]
-            )
-            sum = self.user_product_matrix.sum()
-            num_zeros = total_size - self.user_product_matrix.count_nonzero()
-            data = self.user_product_matrix.multiply(num_zeros / sum)
-        elif weight_strategy == "same":
-            data = self.user_product_matrix
-        else:
-            raise ValueError("Weight strategy not supported")
-
+        data = bm25_weight(
+            self.user_product_matrix,
+            K1=1.2,
+            B=0.75,
+        )
         self.model = _get_model(model_name, **model_params)
         self.fitted = True
-
         self.model.fit(data)
-
         return self
 
     def recommend_products(
@@ -88,42 +63,17 @@ class Recommender:
     ):
         """Finds the recommended items for the user.
         Returns:
-            (items, scores) pair, where item is already the name of the suggested item.
+            (items, scores) pair, of suggested item and score.
         """
 
         if not self.fitted:
             raise InternalStatusError(
-                "Cannot recommend products without previously fitting the model."
-                " Please, consider fitting the model before recommening products."
+                "Fit the model before trying to recommend"
             )
 
         return self.model.recommend(
             user_id,
             self.user_product_matrix[user_id],
-            filter_already_liked_items=True,
+            filter_already_liked_items=False,
             N=items_to_recommend,
         )
-
-    def explain_recommendation(
-            self,
-            user_id,
-            suggested_item_id,
-            recommended_items,
-    ):
-        _, items_score_contrib, _ = self.model.explain(
-            user_id,
-            self.user_product_matrix,
-            suggested_item_id,
-            N=recommended_items,
-        )
-
-        return items_score_contrib
-
-    def similar_users(self, user_id):
-        return self.model.similar_users(user_id)
-
-    @property
-    def item_factors(self):
-        return self.model.item_factors
-
-
