bdf6a742a051d6e176d23fc9877d1efb85462895
Zevs <piszczatowski.artur@gmail.copm>
2022-11-27 22:49:18 +0100

    Cloud etc.

--- ai-diet-generator/src/main/java/pjwstk/aidietgenerator/AiDietGeneratorApplication.java
+++ ai-diet-generator/src/main/java/pjwstk/aidietgenerator/AiDietGeneratorApplication.java
@@ -3,12 +3,19 @@ package pjwstk.aidietgenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class AiDietGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiDietGeneratorApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloPage(){
+		return "Hello";
 	}
 
 }
