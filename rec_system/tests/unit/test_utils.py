6546f18601f9884d3fac22e5cd71ce5c337d8052
unknown <m.kucha@wp.pl>
2022-11-28 15:18:02 +0100

    [SW-45] creating tests

--- rec_system/tests/unit/test_utils.py
+++ rec_system/tests/unit/test_utils.py
@@ -9,5 +9,12 @@ def test_load_and_preprocess_data_with_empty_df(test_input, expected):
                        match='The truth value of a DataFrame is ambiguous..*') as exc_info:
         load_and_preprocess_data(df=test_input)
 
+
+@pytest.mark.parametrize("test_input,expected", [(pd.DataFrame(), None)])
+def test_load_and_preprocess_data_with_empty_csv(test_input, expected):
+    with pytest.raises(ValueError,
+                       match='The truth value of a DataFrame is ambiguous..*') as exc_info:
+        load_and_preprocess_data(df=test_input)
+
 # def test_load_and_preprocess_data_with_csv_path():
 #     dataset = load_and_preprocess_data()
