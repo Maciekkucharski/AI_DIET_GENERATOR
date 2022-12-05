fc70e35208d7683aae59d1bddc0db20d2d90ef17
unknown <s20151@pjwstk.edu.pl>
2022-12-06 00:06:40 +0100

    edit webmvcconfigurer

--- ai-diet-generator/src/main/java/pjwstk/aidietgenerator/security/WebConfiguration.java
+++ ai-diet-generator/src/main/java/pjwstk/aidietgenerator/security/WebConfiguration.java
@@ -2,9 +2,11 @@ package pjwstk.aidietgenerator.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
