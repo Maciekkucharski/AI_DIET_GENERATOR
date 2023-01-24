a3262f73023016b7cb24602331d1336d1d73a682
Zevs <piszczatowski.artur@gmail.copm>
2023-01-24 23:50:03 +0100

    Recipe TOP10LIKEDRECIPES query

--- ai-diet-generator/src/main/java/pjwstk/aidietgenerator/repository/RecipeRepository.java
+++ ai-diet-generator/src/main/java/pjwstk/aidietgenerator/repository/RecipeRepository.java
@@ -1,6 +1,7 @@
 package pjwstk.aidietgenerator.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pjwstk.aidietgenerator.entity.Recipe;
 import pjwstk.aidietgenerator.entity.User;
@@ -15,4 +16,8 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByVerifiedTrueAndUserNotNull();
     List<Recipe> findByVerifiedFalseAndUserNotNull();
     List<Recipe> findFirst10ByUserNotNull();
+
+    @Query(value = "SELECT recipes.id, COUNT(*) AS number_of_likes FROM recipes, recipe_likes WHERE recipes.id = recipe_likes.recipe_id GROUP BY recipes.id ORDER BY number_of_likes DESC LIMIT 10",
+    nativeQuery = true)
+    List<Long> findTopTenLikedRecipes();
 }
